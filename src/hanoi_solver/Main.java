840439f4a96ce44de8e8bb730924a97bd9e790a9
Nick P <nb.piscitello+git@gmail.com>
2016-01-15 09:16:10 -0500

    added instruction print threshold.  Will re-write with CommonsCLI...

--- src/hanoi_solver/Main.java
+++ src/hanoi_solver/Main.java
@@ -7,14 +7,16 @@ public class Main {
 	// constants
 	private final static String USAGE = "Usage: java -jar hanoi_solver.jar [NUMBER OF PIECES]";
 	private final static int NUM_PEGS = 3;
+	private final static int PRINT_THRESH_DEFAULT = 100;
 	
 	// variables
+	private static int print_thresh = PRINT_THRESH_DEFAULT;
 	private static int num_pieces;
 	private static ArrayList<Peg> pegs = new ArrayList<>();
 	private static ArrayList<Piece> pieces = new ArrayList<>();
 	
 	// calculate the lowest number of steps required for a solution
-	private static int calculateSteps(int num_pieces) {
+	private static long calculateSteps(int num_pieces) {
 		if(num_pieces == 1) {
 			return(1);
 		} else {
@@ -61,22 +63,25 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// test the number of supplied arguments
-		if(args.length != 1) {
+		if(args.length != 1 && args.length != 2) {
 			System.err.println("\n" + USAGE);
 			return;
 		}
 		
-		// test the type of the supplied argument
+		// test the type of the supplied argument(s)
 		try {
-			num_pieces = Integer.parseInt(args[0]);
+			if(args.length == 1) {
+				num_pieces = Integer.parseInt(args[0]);
+			} else if(args.length == 2) {
+				print_thresh = Integer.parseInt(args[0]);
+				num_pieces = Integer.parseInt(args[1]);
+			}
+			
 		} catch(NumberFormatException e) {
 			System.err.println("\n" + USAGE + "\nDid you input an integer?");
 			return;
 		}
 
-		// print documentation
-		System.out.println("\nPiece 1 is the smallest and peg A is the farthest to the left.");
-		
 		// create arrays of peg and piece objects
 		for(int i = 0; i < NUM_PEGS; i++) {
 			pegs.add(new Peg(i));
@@ -94,21 +99,33 @@ public class Main {
 			}
 		}
 		
-		// status message
-		if(num_pieces > 1) {
-
-			System.out.println("Initialization successful! Load " + num_pieces + " pieces onto peg A.\n");
-			System.out.println("To play a perfect game, perform these " + calculateSteps(num_pieces) + " moves:");
+		long num_steps = calculateSteps(num_pieces);
+		if(num_steps <= print_thresh) {
+			// if the solution is small enough to be printed...
+			System.out.println("\nPiece 1 is the smallest and peg A is the farthest to the left.");
+			if(num_pieces > 1) {
+				System.out.println("Initialization successful! Load " + num_pieces + " pieces onto peg A.\n");
+				System.out.println("To play a perfect game, perform these " + num_steps + " moves:");
+			} else {
+				System.out.println("Initialization successful! Load the piece onto peg A.\n");
+				System.out.println("To play a perfect game, perform this move:");
+			}
+			
+			// start the recursion: try to move the biggest piece to the last peg
+			movePiece(pieces.size() - 1, pegs.size() - 1);
+			
+			// success!
+			System.out.println("\nYou have completed the Tower of Hanoi! Revel in your glory!\n");
+			return;
 		} else {
-			System.out.println("Initialization successful! Load " + num_pieces + " piece onto peg A.\n");
-			System.out.println("To play a perfect game, perform this move:");
+			// if the solution is too big to be printed...
+			System.out.println("To play a perfect game, " + num_steps + " steps must be performed.");
+			System.out.println("Printing of the instruction set has been disabled due to its length.");
+			if(print_thresh == PRINT_THRESH_DEFAULT) {
+				System.out.println("To override the default threshold (" + PRINT_THRESH_DEFAULT + " steps), use:");
+				System.out.println("\t'java -jar hanoi_solver.jar [THRESHOLD] [NUMBER OF PIECES]");
+			}
+			return;
 		}
-		
-		// start the recursion: try to move the biggest piece to the last peg
-		movePiece(pieces.size() - 1, pegs.size() - 1);
-		
-		// success!
-		System.out.println("\nYou have completed the Tower of Hanoi! Revel in your glory!\n");
-		return;
 	}
 }
